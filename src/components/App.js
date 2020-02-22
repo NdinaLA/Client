40b818152d8f9589eb0677364cc2139c53db6e09
Dandy <dandyjreyes@gmail.com>
2020-02-22 15:25:30 -0800

    Used CSS Grid to style merchandise component. Added this.state names in merchandiseThumbnail.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,27 @@
 import React from "react";
 import MerchThumbnail from "./merchThumbnail/merchandiseThumbnail";
 
+const testingGridStyle = {
+  display: "grid",
+  border: "2px dashed rgba(114, 186, 94, 0.35)",
+  gridTemplateColumns: "1fr 1fr 1fr",
+  gridTemplateRows: "auto",
+  justifyItems: "center"
+};
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <React.Fragment>
-        <div>hello</div>
-        <MerchThumbnail />
+        <div style={testingGridStyle}>
+          <MerchThumbnail />
+          <MerchThumbnail />
+          <MerchThumbnail />
+          <MerchThumbnail />
+        </div>
       </React.Fragment>
     );
   }
