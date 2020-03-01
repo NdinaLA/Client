8915f414dcc3773c0c8271bb57bac6718c9199c2
Jeffrey <renjeffreyy@gmail.com>
2020-03-01 11:01:19 -0800

    commiting to merge Dandy's changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import "./navbar.styles.css"
+import "./navbar.styles.css";
 
-const Navbar = () => (
-    <div>
-        <a className="navLink" href= "#">Dresses</a>
-        <a className="navLink" href= "#">Tops</a>
-        <a className="navLink" href= "#">Nursing</a>
-        <a className="navLink" href= "#">Bottoms</a>
-        <a className="navLink" href= "#">Cardigan</a>
-    </div>
-)
+class Navbar extends React.Component {
+  render() {
+    return (
+        <div>
+            <div className= "navbar">
+                <a className= "navLink" href= "#">Dresses</a>
+                <a className= "navLink" href= "#">Tops</a>
+                <a className= "navLink" href= "#">Nursing</a>
+                <a className= "navLink" href= "#">Bottoms</a>
+                <a className= "navLink" href= "#">Cardigan</a>
+            </div>
+      </div>
+    );
+  }
+}
 
 export default Navbar;
