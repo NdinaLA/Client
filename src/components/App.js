705edd5f8d143978c77ba44cade493badefe8d78
Dandy <dandyjreyes@gmail.com>
2020-03-15 09:59:38 -0700

    add model

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,28 @@
 // Libraries
 import React from "react";
-
-// Componenets
 import MerchThumbnail from "./merchThumbnail/merchandiseThumbnail";
-import Header from "./header/header.component";
-
-import axios from "axios";
-import Footer from "./Footer/Footer";
 
 const testingGridStyle = {
   display: "grid",
+  border: "2px dashed rgba(114, 186, 94, 0.35)",
   gridTemplateColumns: "1fr 1fr 1fr",
   gridTemplateRows: "auto",
   justifyItems: "center"
 };
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    };
-  }
-
-  async grabJsonData() {
-    console.log("user data mounted");
-    const { data } = await axios.get("http://localhost:5005/data/all");
-    this.setState({
-      data: data
-    });
-  }
-
-  componentDidMount() {
-    this.grabJsonData();
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
       <React.Fragment>
-        <Header />
         <div style={testingGridStyle}>
-          {this.state.data.map(info => (
-            <MerchThumbnail key={info.id} dataMap={info} />
-          ))}
+          <MerchThumbnail />
+          <MerchThumbnail />
+          <MerchThumbnail />
+          <MerchThumbnail />
         </div>
-        <Footer />
       </React.Fragment>
     );
   }
--- src/components/merchThumbnail/merchandiseThumbnail.js
+++ src/components/merchThumbnail/merchandiseThumbnail.js
@@ -1,22 +1,56 @@
 import React from "react";
-import "./Merch.css";
 
+// Style Place holder to be moved to its own CSS file
+const dot = {
+  height: "25px",
+  width: "25px",
+  backgroundColor: "blue",
+  borderRadius: "50%",
+  display: "inline-block"
+};
+const blockDisplay = {
+  display: "block"
+};
+const imageWidth = {
+  width: "100px"
+};
+const divAlign = {
+  display: "grid",
+  alignItems: "center",
+  justifyItems: "center",
+  backgroundColor: "red",
+  width: "150px",
+  height: "300px",
+  margin: "20px"
+};
 class MerchThumbnail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: "Company Name",
+      itemDescription: "This describes the item",
+      itemPrice: "Price"
+    };
+  }
   render() {
-    // Destructure and pull the variables from database
-    const { img: imageURL, company, description, price } = this.props.dataMap;
+    // Temporarily set variable names for readability
+    const { itemDescription, company, itemPrice } = this.state;
     return (
-      <div className="divAlign">
-        <img className="imageWidth" src={imageURL} alt="" />
+      <div style={divAlign}>
+        <img
+          style={imageWidth}
+          src="https://ae01.alicdn.com/kf/HTB144qlXe6sK1RjSsrbq6xbDXXaq.jpg"
+          alt=""
+        />
         <h3>{company}</h3>
-        {description}
-        <div className="blockDisplay">
-          <span className="dot" />
-          <span className="dot" />
-          <span className="dot" />
-          <span className="dot" />
+        {itemDescription}
+        <div style={blockDisplay}>
+          <span style={dot}></span>
+          <span style={dot}></span>
+          <span style={dot}></span>
+          <span style={dot}></span>
         </div>
-        <h4>{price}</h4>
+        <h4>{itemPrice}</h4>
       </div>
     );
   }
