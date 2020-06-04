adb0d6abe78894ae01cd1a74d45c1d87a865c3c8
Jeffrey <renjeffreyy@gmail.com>
2020-06-04 12:53:10 -0700

    cleaned up code and moved searchbar to its own component

--- src/components/NavbarUserUtils/navbarUserUtils.component.jsx
+++ src/components/NavbarUserUtils/navbarUserUtils.component.jsx
@@ -3,18 +3,16 @@ import React from 'react';
 import './navbarUserUtils.styles.css';
 
 //icons
-import searchIcon from '../../assets/search-icon.svg';
 import cartIcon from '../../assets/cart.svg';
 
+//components
 import LoginModal from '../LoginModal/LoginModal';
+import SearchBar from '../SearchBar/searchBar.component';
+
+//redux
 import { connect } from 'react-redux';
 import { toggleModal } from '../../actions/modalDisplay';
 import { signOutUser } from '../../actions/isLogged';
-import {
-  toggleSearchbar,
-  searchProducts,
-  searchStateToggle,
-} from '../../actions/search';
 
 class NavbarUserUtils extends React.Component {
   constructor(props) {
@@ -38,57 +36,18 @@ class NavbarUserUtils extends React.Component {
   };
 
   initSearch = (event) => {
+    const { toggleSearchbar } = this.props;
     this.onSubmit(event);
-    this.props.toggleSearchbar();
+    toggleSearchbar();
   };
 
   render() {
-    const {
-      toggleModal,
-      authorized,
-      signOutUser,
-      toggleSearchbar,
-      displaySearchbar,
-    } = this.props;
+    const { toggleModal, authorized, signOutUser } = this.props;
 
     return (
       <div className="navbar-user-utils">
         <LoginModal />
-        {displaySearchbar ? (
-          <>
-            <form className="searchForm" onSubmit={this.onSubmit}>
-              <div className="selectContainer">
-                <select className="searchSelector">
-                  <option value="All">All</option>
-                  <option value="placeholder">placeholder</option>
-                  <option value="placeholder">placeholder</option>
-                </select>
-              </div>
-              <input
-                className="searchInput"
-                name="searchInput"
-                value={this.state.searchInput}
-                onChange={this.onChange}
-              />
-              <input type="submit" className="submit" />
-            </form>
-            <div className="graySearch">
-              <img
-                className="navbar-user-utils-icon searchIcon"
-                src={searchIcon}
-                alt="search icon"
-                onClick={this.initSearch}
-              />
-            </div>
-          </>
-        ) : (
-          <img
-            className="navbar-user-utils-icon"
-            src={searchIcon}
-            alt="search icon"
-            onClick={toggleSearchbar}
-          />
-        )}
+        <SearchBar />
 
         {authorized === false ? (
           <p className="navbar-user-utils-icon" onClick={toggleModal}>
@@ -112,14 +71,10 @@ class NavbarUserUtils extends React.Component {
 const mapStateToProps = (state) => {
   return {
     authorized: state.isLogged.authorized,
-    displaySearchbar: state.searchProducts.displaySearchbar,
   };
 };
 
 export default connect(mapStateToProps, {
   toggleModal,
   signOutUser,
-  toggleSearchbar,
-  searchProducts,
-  searchStateToggle,
 })(NavbarUserUtils);
